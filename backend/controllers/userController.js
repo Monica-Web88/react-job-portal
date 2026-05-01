9a0a47109a55234bacd6e2abd6a0fa8af19d6e79
Monica-Web88 <itsme.cloudy3@gmail.com>
2026-05-01 13:42:22 -0700

    Updated the toast message

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,7 +25,7 @@ export const register = catchAsyncErrors(async (req, res, next) => {
 export const login = catchAsyncErrors(async (req, res, next) => {
   const { email, password, role } = req.body;
   if (!email || !password || !role) {
-    return next(new ErrorHandler("Please provide email ,password and role !"));
+    return next(new ErrorHandler("Please provide email, password and role!"));
   }
   const user = await User.findOne({ email }).select("+password");
   if (!user) {
@@ -33,11 +33,11 @@ export const login = catchAsyncErrors(async (req, res, next) => {
   }
   const isPasswordMatched = await user.comparePassword(password);
   if (!isPasswordMatched) {
-    return next(new ErrorHandler("Invalid Email Or Password !", 400));
+    return next(new ErrorHandler("Invalid Email Or Password!", 400));
   }
   if (user.role !== role) {
     return next(
-      new ErrorHandler(`User with provided email and ${role} not found !`, 404)
+      new ErrorHandler(`User with provided email and ${role} not found!`, 404)
     );
   }
   sendToken(user, 201, res, "User Logged In Sucessfully !");
